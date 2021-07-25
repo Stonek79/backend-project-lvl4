b7f58d175f62906f724ef7eee5b35db958e23545
Stonek79 <amit-yurist@mail.ru>
2021-07-25 14:50:16 +0300

    Try add db heroku

--- knexfile.js
+++ knexfile.js
@@ -27,13 +27,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      host: process.env.DB_HOST,
-    },
+    connection: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     useNullAsDefault: true,
     migrations,
