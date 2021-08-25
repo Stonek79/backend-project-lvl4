e08f793594a1575489d70233f229f12a4ddcbeb9
Stonek79 <amit-yurist@mail.ru>
2021-08-25 14:25:16 +0300

    Change knexfile

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URI,
+    connection: {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      ssl: { rejectUnauthorized: false },
+    },
     useNullAsDefault: true,
     migrations,
   },
